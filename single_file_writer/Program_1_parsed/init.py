e9f6c5c9a3d2a86421acc11c6d2c59463780b304
Sqh3rd <moitz417@gmail.com>
2022-10-03 20:56:46 +0200

    Extract file compacter to extern repo

--- single_file_writer/Program_1_parsed/init.py
+++
@@ -1,5 +0,0 @@
-files={'auxilary.py': [], 'main.py': ['from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair\n', 'from spike.control import wait_for_seconds, wait_until, Timer\n', '\n', 'def main():\n', '    hub = PrimeHub()\n', '    while True:\n', "        hub.light_matrix.show_image('HAPPY')\n", '        wait_for_seconds(5)\n', "        hub.light_matrix.show_image('SAD')\n", '        wait_for_seconds(5)']}
-for k in files:
-	with open(f'./{k}', 'w') as f:
-		f.write('\n'.join(files[k]))
-exec('./main.py')
